cce1f52a8b6aa7b25fe7cc6fa94cee2332ff9e9c
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-05 12:34:44 +0900

    add: 재귀로 구현한 이진 탐색 알고리즘 추가

--- src/algorithms/searching/binarySearch/binarySearch.js
+++ src/algorithms/searching/binarySearch/binarySearch.js
@@ -29,3 +29,15 @@ export function binarySearchDsc(arr, searchTarget) {
 
 	return -1;
 }
+
+// 배열이 오름차순으로 정렬되어있다고 가정
+export function recursiveBinarySearch(arr, target, lo = 0, hi = arr.length - 1) {
+	if (lo > hi) return -1;
+
+	const mid = lo + Math.floor((hi - lo) / 2);
+	const currentElem = arr[mid];
+
+	if (currentElem === target) return mid;
+	if (currentElem < target) return recursiveBinarySearch(arr, target, mid + 1, hi);
+	else return recursiveBinarySearch(arr, target, lo, mid - 1);
+}
--- src/algorithms/searching/binarySearch/binarySearch.test.js
+++ src/algorithms/searching/binarySearch/binarySearch.test.js
@@ -1,4 +1,4 @@
-import { binarySearchAsc, binarySearchDsc } from './binarySearch';
+import { binarySearchAsc, binarySearchDsc, recursiveBinarySearch } from './binarySearch';
 
 test('Should find a number in ascending array', () => {
 	expect(binarySearchAsc([], 1)).toBe(-1);
@@ -26,4 +26,18 @@ test('Should find a number in descending array', () => {
 	expect(binarySearchDsc([100, 1, 0, -1, -4, -5, -7], 100)).toBe(0);
 	expect(binarySearchDsc([100, 1, 0, -1, -4, -5, -7], 1000)).toBe(-1);
 	expect(binarySearchAsc([1, 1, 1, 1], 1)).toBe(1);
+});
+
+test('Should find a number in ascending array by using recursive version', () => {
+	expect(recursiveBinarySearch([], 1)).toBe(-1);
+	expect(recursiveBinarySearch([1], 1)).toBe(0);
+	expect(recursiveBinarySearch([1, 2], 1)).toBe(0);
+	expect(recursiveBinarySearch([1, 2], 2)).toBe(1);
+	expect(recursiveBinarySearch([-5, -4, -2], -2)).toBe(2);
+	expect(recursiveBinarySearch([-5, -4, -2, 0, 3, 5], -5)).toBe(0);
+	expect(recursiveBinarySearch([-5, -4, -2, 0, 3, 5], 3)).toBe(4);
+	expect(recursiveBinarySearch([-5, -4, -2, 0, 3, 5], -8)).toBe(-1);
+	expect(recursiveBinarySearch([-5, -4, -2, 0, 3, 5, 100], 100)).toBe(6);
+	expect(recursiveBinarySearch([-5, -4, -2, 0, 3, 5, 100], 1000)).toBe(-1);
+	expect(recursiveBinarySearch([1, 1, 1, 1], 1)).toBe(1);
 });
